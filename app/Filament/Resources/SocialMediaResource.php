bdf8c488aa2db35dbda406827e8c68e913763d9f
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2025-10-06 13:44:35 +0700

    CRUD social media

---
+++ app/Filament/Resources/SocialMediaResource.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SocialMediaResource\Pages;
+use App\Models\SocialMedia;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Forms\Components\FileUpload;
+
+class SocialMediaResource extends Resource
+{
+    protected static ?string $model = SocialMedia::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-share';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                FileUpload::make('logo')
+                    ->required()
+                    ->image()
+                    ->disk('public')
+                    ->directory('social-media-logos')
+                    ->label('Logo'),
+
+                Forms\Components\TextInput::make('url')
+                    ->required()
+                    ->maxLength(255)
+                    ->url()
+                    ->label('URL')
+                    ->placeholder('https://www.example.com'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                ImageColumn::make('logo')
+                    ->label('Logo'),
+
+                Tables\Columns\TextColumn::make('url')
+                    ->searchable()
+                    ->label('URL'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSocialMedia::route('/'),
+            'create' => Pages\CreateSocialMedia::route('/create'),
+            'edit' => Pages\EditSocialMedia::route('/{record}/edit'),
+        ];
+    }
+}
