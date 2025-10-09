08832fa6979c6b5892091f1fc9cda8eb9b50e65b
kucingcoder <137396727+kucingcoder@users.noreply.github.com>
2025-10-09 15:03:48 +0700

    Apply template

---
+++ resources/views/layouts/landing_js.blade.php
@@ -0,0 +1,7 @@
+<script src="/landing/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/landing/vendor/php-email-form/validate.js"></script>
+<script src="/landing/vendor/aos/aos.js"></script>
+<script src="/landing/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="/landing/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="/landing/vendor/purecounter/purecounter_vanilla.js"></script>
+<script src="/landing/js/main.js"></script>
